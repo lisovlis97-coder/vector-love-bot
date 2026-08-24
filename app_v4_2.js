5a88a8f0af72494d63890b305e88b1e3d6235a97
lisovlis97-coder <lisov.lis97@gmail.com>
2026-08-24 21:43:51 +0300

    Fix VK photo attachment patch

--- app_v4_2.js
+++ app_v4_2.js
@@ -5,17 +5,15 @@ const Module = require("module");
 const wrapperPath = path.join(__dirname, "app_v4_1.js");
 let wrapperSource = fs.readFileSync(wrapperPath, "utf8");
 
+const marker = 'const patched = new Module(sourcePath, module.parent);';
 const injection = `
 source = source.replace(
-  'function getPhotoAttachment(vkMessage) {\\n  const photoAttachment = (vkMessage.attachments || []).find(item => item.type === "photo");\\n  if (!photoAttachment) return null;\\n  const photo = photoAttachment.photo;\\n  return \\`photo\\${photo.owner_id}_\\${photo.id}\\`;\\n}',
-  'function getPhotoAttachment(vkMessage) {\\n  const photoAttachment = (vkMessage.attachments || []).find(item => item.type === "photo");\\n  if (!photoAttachment) return null;\\n  const photo = photoAttachment.photo;\\n  const access = photo.access_key ? \\`_\\${photo.access_key}\\` : "";\\n  return \\`photo\\${photo.owner_id}_\\${photo.id}\\${access}\\`;\\n}'
+  /function getPhotoAttachment\\(vkMessage\\) \\{[\\s\\S]*?\\n\\}/,
+  'function getPhotoAttachment(vkMessage) {\\n  const photoAttachment = (vkMessage.attachments || []).find(item => item.type === "photo");\\n  if (!photoAttachment) return null;\\n  const photo = photoAttachment.photo;\\n  return "photo" + photo.owner_id + "_" + photo.id + (photo.access_key ? "_" + photo.access_key : "");\\n}'
 );
 `;
 
-wrapperSource = wrapperSource.replace(
-  'const patched = new Module(sourcePath, module.parent);',
-  injection + '\nconst patched = new Module(sourcePath, module.parent);'
-);
+wrapperSource = wrapperSource.replace(marker, injection + "\n" + marker);
 
 const patchedWrapper = new Module(wrapperPath, module.parent);
 patchedWrapper.filename = wrapperPath;
